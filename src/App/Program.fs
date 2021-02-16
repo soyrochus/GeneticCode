f887f0c7e45d579e3de25e52efac29bf6ed6a07d
soyrochus <iwan-rochus.van-der-kleijn@capgemini.com>
2021-02-16 14:06:04 +0100

    Added NUnit etc. to App-fsproj

--- src/App/Program.fs
+++ src/App/Program.fs
@@ -1,12 +1,9 @@
 // Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
-
 open System
+
 // Define a function to construct a message to print
-let from whom =
-    sprintf "from %s" whom
+open GenCode.Encoder
 
 [<EntryPoint>]
 let main argv =
-    let message = from "F#" // Call the function
-
-    0 // return an integer exit code
+    0
